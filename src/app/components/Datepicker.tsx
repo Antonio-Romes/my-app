71cc638501213f60447f34218acf463071da5db9
Antonio-Romes <antonioromes1@hotmail.com>
2023-11-14 16:38:31 -0300

    Foi criado o component DatePicker e ajustado o layout dos input de cadastro.

---
+++ src/app/components/Datepicker.tsx
@@ -0,0 +1,31 @@
+'use client';
+
+ 
+import React, { useState } from 'react';
+import DatePicker, { registerLocale } from "react-datepicker"; 
+import ptBR from 'date-fns/locale/pt-BR';
+registerLocale('pt-br',ptBR)
+
+import 'react-datepicker/dist/react-datepicker.css';
+import { format } from 'date-fns';
+
+export  default function  MyDatePicker() {
+    const [selectedDate, setSelectedDate] = useState( new Date() ); 
+   
+    return (
+        <>
+            <label htmlFor="birthDate" className="block text-lg font-bold leading-6 text-gray-900">Data Nascimento: </label>
+            <div className="mt-2"> 
+                <DatePicker className=' w-72 block rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 p-2'
+                selected={ selectedDate }
+                onChange={(date:Date) => setSelectedDate( date)}
+                dateFormat="dd/MM/yyyy"  
+                locale="pt-br" 
+                />
+             </div>
+        
+        </>
+        
+       
+    );
+   }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,103 +1,86 @@
-import Image from 'next/image'
+'use client';
+
+import MyDatePicker from "./components/Datepicker";
+
+ 
+ 
 
 export default function Home() {
   return (
-     <main className='h-screen w-full bg-zinc-500'>
+     <main className='h-screen w-full bg-zinc-200'>
         <div className='w-full flex justify-center'>
-             <section className='w-3/4 bg-red-200 border border-s-black rounded-xl pt-8 my-8'>
+             <section className='w-3/4 border border-black rounded-xl pt-8 my-8'>
                 <h1 className='text-center text-6xl'>Cadastro</h1>
                 <div className='flex justify-center mt-5 '>
-                 <hr className='border border-solid rounded-full w-2/4' />
+                 <hr className='border border-solid h-1 bg-black rounded-full w-4/5' />
                 </div> 
                 <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-2 sm:grid-cols-6 m-6">
                     <div className="sm:col-span-4">
-                        <label htmlFor="first-name" className="block text-sm font-medium leading-6 text-gray-900">First name</label>
+                        <label htmlFor="name" className="block text-lg font-bold leading-6 text-gray-900">Nome: </label>
                         <div className="mt-2">
-                            <input type="text" name="first-name" id="first-name" autoComplete="given-name" className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
+                            <input type="text" name="name" id="name" autoComplete="given-name" placeholder='Nome' className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 p-2" />
                         </div>
                     </div> 
                     <div className="sm:col-span-2">
-                        <label htmlFor="last-name" className="block text-sm font-medium leading-6 text-gray-900">Last name</label>
-                        <div className="mt-2">
-                            <input type="text" name="last-name" id="last-name" autoComplete="family-name" className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
-                        </div>
+                         <MyDatePicker />
                     </div>
                     <div className="sm:col-span-4">
-                        <label htmlFor="first-name" className="block text-sm font-medium leading-6 text-gray-900">First name</label>
+                        <label htmlFor="email" className="block text-lg font-bold leading-6 text-gray-900">Email: </label>
                         <div className="mt-2">
-                            <input type="text" name="first-name" id="first-name" autoComplete="given-name" className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
+                            <input type="email" name="email" id="email" autoComplete="given-name" placeholder='Digite o Email' className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 p-2" />
                         </div>
                     </div> 
                     <div className="sm:col-span-2">
-                        <label htmlFor="last-name" className="block text-sm font-medium leading-6 text-gray-900">Last name</label>
+                        <label htmlFor="Profession" className="block text-lg font-bold leading-6 text-gray-900">Profissão: </label>
                         <div className="mt-2">
-                            <input type="text" name="last-name" id="last-name" autoComplete="family-name" className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
+                            <select id="Profession" name="country" autoComplete="country-name" className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:max-w-xs sm:text-sm sm:leading-6 p-2">
+                                <option> - Selecione - </option>
+                                <option>Arquiteto de redes</option>
+                                <option>Analista de big data</option> 
+                                <option>Analista de testes de softwares</option>
+                                <option>Ciência de dados</option> 
+                                <option>Desenvolvimento de games</option>
+                                <option>Desenvolvedor mobile</option>
+                                <option>Engenharia de software </option>
+                                <option>Programador web</option> 
+                                <option>Segurança da informação</option>
+                            </select>
                         </div>
                     </div>
-                    <div className="sm:col-span-6 mt-6 flex items-center justify-end gap-x-6"> 
-                        <button type="submit" className="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Save</button>
+                    <div className='flex justify-center mt-5 sm:col-span-6'>
+                         <hr className='border border-solid h-1 bg-black rounded-full w-full' />
+                    </div> 
+                    <div className="sm:col-span-6 mt-2 flex items-center justify-end gap-x-6"> 
+                        <button type="submit" className="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Salvar</button>
                     </div>
                 </div>
              </section>
         </div>
-        <div className='w-full bg-blue-600'>
+        <div className='w-full'>
             <div className="relative overflow-x-auto m-3">
-                <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+                <table className="w-full text-sm text-left  text-gray-500 dark:text-gray-400">
                     <thead className="text-xs text-gray-700 uppercase bg-gray-100 dark:bg-gray-700 dark:text-gray-400">
                         <tr>
-                            <th scope="col" className="px-6 py-3 rounded-s-lg">
-                                Product name
+                            <th scope="col" className="px-6 py-3 text-center">
+                                Nome
                             </th>
-                            <th scope="col" className="px-6 py-3">
-                                Qty
+                            <th scope="col" className="px-6 py-3  text-center">
+                                Email
                             </th>
-                            <th scope="col" className="px-6 py-3 rounded-e-lg">
-                                Price
+                            <th scope="col" className="px-6 py-3  text-center">
+                                Profissão
+                            </th>
+                            <th scope="col" className="px-6 py-3  text-center">
+                                Data Nascimento
                             </th>
                         </tr>
                     </thead>
                     <tbody>
-                        <tr className="bg-white dark:bg-gray-800">
-                            <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                                Apple MacBook Pro 17"
-                            </th>
-                            <td className="px-6 py-4">
-                                1
-                            </td>
-                            <td className="px-6 py-4">
-                                $2999
-                            </td>
-                        </tr>
-                        <tr className="bg-white dark:bg-gray-800">
-                            <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                                Microsoft Surface Pro
-                            </th>
-                            <td className="px-6 py-4">
-                                1
-                            </td>
-                            <td className="px-6 py-4">
-                                $1999
-                            </td>
-                        </tr>
-                        <tr className="bg-white dark:bg-gray-800">
-                            <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                                Magic Mouse 2
-                            </th>
-                            <td className="px-6 py-4">
-                            1
-                            </td>
-                            <td className="px-6 py-4">
-                                $99
-                            </td>
-                        </tr>
+                         <div className="bg-red-700">
+                         <h1 className='text-slate-900 text-center'> Sem valor</h1>
+                         </div> 
                     </tbody>
-                    <tfoot>
-                        <tr className="font-semibold text-gray-900 dark:text-white">
-                            <th scope="row" className="px-6 py-3 text-base">Total</th>
-                            <td className="px-6 py-3">3</td>
-                            <td className="px-6 py-3">21,000</td>
-                        </tr>
-                    </tfoot>
+                    
                 </table>
             </div>
         </div>
